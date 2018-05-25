92b79d1c78ef74683c5261a4ac0565928ed3716c
z29275164 <zz29275164@gmail.com>
2018-05-25 15:34:59 +0800

    指向非常數資料的非常數指標

---
+++ BubbleSort.c
@@ -0,0 +1,21 @@
+#define SIZE 10
+void bubbleSort(int * array, unsigned int size )
+	{
+		void swap(int *element1Ptr, int *element2Ptr );
+		unsigned int pass;/*declare*/
+		unsigned int j;/*declare*/
+		
+	for (pass= 0;pass<SIZE;++pass){
+	    for(j=0;j<size-1;++j){
+				if(array[j]>array[j+1]){
+					swap(&array[j],&array[j+1]);		
+				}/*end if*/
+			}/*end inner for*/
+		}/*end out for*/
+}/*end function bubblesort*/
+void swap(int *element1Ptr,int *element2Ptr)
+{
+	int hold=*element1Ptr;
+	*element1Ptr=*element2Ptr;
+	*element2Ptr= hold;
+}/*end function swap*/
---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 10
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void bubbleSort( int*array, unsigned int size );/*prototye*/
+
+int main(void) {
+	int a[SIZE]={2,10,5,30,8,19,22,16,3,7};/*initlize arry a*/
+	
+	unsigned int i; /*declare*/
+	
+	puts(" before");
+	
+		for(i=0;i<SIZE;++i){
+			printf("%4d ",a[i]);
+		}/*end for*/
+		
+		bubbleSort(a, SIZE);/*sort the array*/
+		
+		puts("\n after");/*loop through*/
+		
+		for(i=0;i<SIZE;++i){
+			printf("%4d ",a[i]);
+	    }
+		
+		puts("");
+}/*end main*/
