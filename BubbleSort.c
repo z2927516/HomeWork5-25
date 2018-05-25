343cbb1b4fddf6e3af01c0797eb6d615f3c9e187
z29275164 <zz29275164@gmail.com>
2018-05-25 15:45:15 +0800

    指向常數資料的常數指標

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,5 +1,5 @@
 #define SIZE 10
-void bubbleSort(const int * array, unsigned int size )
+void bubbleSort(const int *const array, unsigned int size )
 	{
 		void swap(int *element1Ptr, int *element2Ptr );
 		unsigned int pass;/*declare*/
